0a74f6b9203ec87a4583b8f2ea59fb8171140748
Nicolas-Ortiz <nicortiz1234@gmail.com>
2021-05-23 00:16:03 -0300

    nueva implementacion para las validaciones del modelo Question

--- models/question.rb
+++ models/question.rb
@@ -2,11 +2,9 @@ class Question < Sequel::Model
 	one_to_many :responses
 	one_to_many :choices
 
+	plugin :validation_helpers
 	def validate
 		super	
-		errors.add(:name, 'cannot be empty') if !name || name.empty?
-		errors.add(:number, 'cannot be empty') if !number || number==nil
-		errors.add(:description, 'cannot be empty') if !description || description.empty?
-		errors.add(:type, 'cannot be empty') if !type || type.empty?
+		validates_presence [:name, :description, :number, :type]
 	end
 end
