08f7c2d67e0f559d10285e6abfcd2c2e040560fa
Nicolas-Ortiz <nicortiz1234@gmail.com>
2021-05-24 19:05:36 -0300

    Validaciones para modelo Career

--- models/career.rb
+++ models/career.rb
@@ -1,4 +1,10 @@
 class Career < Sequel::Model
 	one_to_many :outcomes
 	one_to_many :surveys
+
+	plugin :validation_helpers
+    def validate
+        super
+            validates_presence [:name]
+    end
 end
